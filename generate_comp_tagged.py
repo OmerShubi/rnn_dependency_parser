ccc0d7bc898c85b96b0a614c8cbf6d5bebf47a85
Nitzan Shamir <shamirnitzan@campus.htecnion.ac.il>
2020-06-06 19:05:14 +0300

    fix create comp file

--- generate_comp_tagged.py
+++ generate_comp_tagged.py
@@ -9,27 +9,20 @@ from torch import load
 from utils.DataPreprocessing import *
 
 
-TAG_EMBEDDING_DIM = 25
-WORD_EMBEDDING_DIM = 100
-EPOCHS = 5
-LR = 0.01
-ARTIFICIAL_BATCH = 50
-# ARTIFICIAL_BATCH = 5
-
 def main():
     path_model1 = "models/model1.pth"
     # path_model2 = "models/model2.pth"
     model = load(path_model1)
     word_dict, tag_dict = model.word_dict, model.tag_dict
     path_comp = "Data/comp.unlabeled"
-    tagged_path_comp = "comp_m{}_206348187.labeled" # TODO change name by req
+    tagged_path_comp = "comp_m_206348187.labeled" # TODO change name by req
     comp = DepDataset(word_dict,
                       tag_dict,
                       path_comp,
                       comp=True)
     comp_dataloader = DataLoader(comp, shuffle=False)
     new_sentences = comp_infer(model, comp_dataloader)
-    comp_writer(new_sentences, tagged_path_comp)
+    comp_writer(new_sentences, tagged_path_comp, path_comp)
 
 def comp_infer(model, comp_dataloader):
     sentences = comp_dataloader.dataset.sentences_dataset
@@ -38,14 +31,20 @@ def comp_infer(model, comp_dataloader):
         sentences[batch_idx] = input_data[0], input_data[1], predicted_tree_heads
     return sentences
 
-def comp_writer(sentences, file_path):
+def comp_writer(sentences, file_path, original_file_path):
+    with open (original_file_path, "r") as f:
+        originial_data = f.readlines()
+    num_lines = 0
     with open(file_path, "w") as f:
         for sentence in sentences:
-            words, tags, heads = sentence
-            # heads.size(0) num of words in sentence
-            for i in range(1, words.size(0)):
-                f.write(get_line(i, words[i], tags[i], heads[i]))
+            _, _, heads = sentence
+            # heads.size(1) num of words in sentence
+            for i in range(1, heads.size(0)):
+                original_sentence = originial_data[num_lines].split("\t")
+                f.write(get_line(i, original_sentence[1], original_sentence[3], heads[i]))
+                num_lines += 1
             f.write("\n")
+            num_lines += 1
 
 
 def get_line(num, word, pos, head):
